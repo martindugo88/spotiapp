b6d064fbaa53aa3e0fb9cd4a4f34098238cfeba3
Martin Dugo <martindugo88@gmail.com>
2018-03-16 17:57:23 -0300

    updating spotiapp

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -9,6 +9,7 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class ArtistComponent implements OnInit {
   artista:any = [];
+  songs:any[] = [];
 
   constructor(private activatedRoute:ActivatedRoute, private _spotify:SpotifyService) { }
 
@@ -19,9 +20,13 @@ export class ArtistComponent implements OnInit {
             .subscribe( artist => {
               console.log(artist);
               this.artista = artist;
-            }
+        });
 
-        );
+        this._spotify.getTop(params['id'])
+            .subscribe( tracks => {
+              console.log(tracks);
+        });
+
     })
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
+
+
 @Injectable()
 export class SpotifyService {
   artistas:any[] = [];
-  artista:any = {};
+  token:string = "Bearer BQATlk4pHB_8pBoAItXicajC8Xsf7I0KhlDzmqBoThuiIMAA7cAAc65nkmfW15Ln8DU7VV2djMtKas8k4eo";
 
   constructor( public http : HttpClient ) {
     console.log("Servicio de spotify listo");
@@ -15,10 +17,10 @@ export class SpotifyService {
     let url = `https://api.spotify.com/v1/search?query=${ termino }&type=artist&offset=0&limit=20`;
 
     let headers = new HttpHeaders({
-      'authorization' : "Bearer BQCyZGKIixvMuU5U3zhJTyX9YrZoVc9eqWmc5HB7uEm7wwHU686whI7ykbLX38U_zBpiAvrnKXR7plgeocY"
+      'authorization' : this.token
     })
 
-    return this.http.get(url, { headers }).map( resp =>{
+    return this.http.get(url, { headers }).map( (resp:any) => {
       this.artistas = resp.artists.items;
       return this.artistas;
     })
@@ -28,11 +30,22 @@ export class SpotifyService {
     let url = `https://api.spotify.com/v1/artists/${ id }`;
 
     let headers = new HttpHeaders({
-      'authorization' : "Bearer BQCyZGKIixvMuU5U3zhJTyX9YrZoVc9eqWmc5HB7uEm7wwHU686whI7ykbLX38U_zBpiAvrnKXR7plgeocY"
+      'authorization' : this.token
     })
 
     return this.http.get(url, { headers });
 
+  }
+
+  getTop( id:string ){
+    let url = `https://api.spotify.com/v1/artists/${ id }/top-tracks?country=ES`;
+
+    let headers = new HttpHeaders({
+      'authorization' : this.token
+    })
+
+    return this.http.get(url, { headers })
+            .map( (resp:any) => (resp.track));
   }
 
 }
